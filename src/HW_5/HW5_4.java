a996dc99a112bbaacb3990ebe428de5d095f66a8
HP <bilimus@gmail.com>
2021-03-20 16:30:54 +0500

    hw

--- src/HW_5/HW5_4.java
+++ src/HW_5/HW5_4.java
@@ -4,17 +4,15 @@
 //int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9};
 //необходимо вывести среднее арифметическое всех значений массива.
 
-
-
 package HW_5;
 
 public class HW5_4 {
     public static void main(String[] Args){
-        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-        int sum = 0;
+        int[] array = {1, 21, 31, 41, 51, 61, 71, 8, 9};
+        double sum = 0;
         for (int j : array) {
             sum += j;
         }
-        System.out.println("Avg is: " + sum/array.length * 1.0);
+        System.out.println("Avg is: " + sum / array.length);
     }
 }
