ecf76677a3441117297974ac59749562bb3f1808
HP <bilimus@gmail.com>
2021-03-20 17:11:21 +0500

    hw

--- src/HW_6/HW_6_extra.java
+++ src/HW_6/HW_6_extra.java
@@ -9,6 +9,15 @@ package HW_6;
 public class HW_6_extra {
     public static void main(String[] args) {
         String[][] array = {{"Привет", "всем", "кто"}, {"изучает", "язык", "программирования"}, {"java"}};
+            int count = 0;
+            char ch = 'е';
 
-    }
+            for (int i =0; i < array.length; i++){
+                for (int j = 0; j < array[i].length; j++){
+                    if(array[i][j].indexOf(ch) == -1)
+                        count++;
+                }
+            }
+        System.out.println(count);
+   }
 }
